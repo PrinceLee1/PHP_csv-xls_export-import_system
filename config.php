a0b6ad63c62edb81a3b4f0406c71a95f5fbb1266
Onyebuchi Prince <49554317+PrinceLee1@users.noreply.github.com>
2020-04-26 20:19:23 +0100

    Update config.php

--- config.php
+++ config.php
@@ -9,10 +9,10 @@ require_once 'vendor/autoload.php';
 $google_client = new Google_Client();
 
 //Set the OAuth 2.0 Client ID
-$google_client->setClientId('718246433673-o7l2pjm6lbe0stg08mb9157kmu5br2rq.apps.googleusercontent.com');
+$google_client->setClientId('');
 
 //Set the OAuth 2.0 Client Secret key
-$google_client->setClientSecret('TfiaWZbpDRKZLjFjISLvnS8_');
+$google_client->setClientSecret('');
 
 //Set the OAuth 2.0 Redirect URI
 $google_client->setRedirectUri('http://localhost/single-page-system/index.php');
